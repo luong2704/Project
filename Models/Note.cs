f72e2a62b0f3ed55eaaa2d52d7f2e09ac7f2ed6e
hieu26 <dtc225190011@ictu.edu.vn>
2025-03-11 09:23:25 +0700

    using dtb

--- Models/Note.cs
+++ Models/Note.cs
@@ -1,51 +1,97 @@
-﻿namespace Notes.Models;
+﻿using Microsoft.Graph;
+using Notes.Data.Repository;
+
+namespace Notes.Models;
 
 internal class Note
 {
     public string Filename { get; set; }
     public string Text { get; set; }
     public DateTime Date { get; set; }
+
+    private INoteRepository _noteRepository;
     public Note()
     {
         Filename = $"{Path.GetRandomFileName()}.notes.txt";
         Date = DateTime.Now;
         Text = "";
+        _noteRepository = new NoteRepository();
     }
-    public void Save() =>
-File.WriteAllText(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename), Text);
+//    public void Save() =>
+//File.WriteAllText(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename), Text);
+    public void Save()
+    {
+        var item = _noteRepository.ReadItem(Filename);
+        if (item is null)
+        {
+            _noteRepository.Write(Filename, Text);
+        }
+        else
+        {
+            item.Data = Text;
+            _noteRepository.Update(item.Id, Filename, Text);
+        }
+    }
+    public void Delete()
+    {
+        var item = _noteRepository.ReadItem(Filename);
+        if (item is not null)
+        {
+            _noteRepository.Delete(Filename, Text);
+        }
+    }
+    //public void Delete() =>
+    //    File.Delete(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename));
+    //public static Note Load(string filename)
+    //{
+    //    filename = System.IO.Path.Combine(FileSystem.AppDataDirectory, filename);
 
-    public void Delete() =>
-        File.Delete(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename));
+    //    if (!File.Exists(filename))
+    //        throw new FileNotFoundException("Unable to find file on local storage.", filename);
+
+    //    return
+    //        new()
+    //        {
+    //            Filename = Path.GetFileName(filename),
+    //            Text = File.ReadAllText(filename),
+    //            Date = File.GetLastWriteTime(filename)
+    //        };
+    //}
     public static Note Load(string filename)
     {
-        filename = System.IO.Path.Combine(FileSystem.AppDataDirectory, filename);
-
-        if (!File.Exists(filename))
-            throw new FileNotFoundException("Unable to find file on local storage.", filename);
-
-        return
-            new()
-            {
-                Filename = Path.GetFileName(filename),
-                Text = File.ReadAllText(filename),
-                Date = File.GetLastWriteTime(filename)
-            };
+        var item = new NoteRepository().ReadItem(filename);
+        return new Note
+        {
+            Filename = item.Name,
+            Text = item.Data,
+            Date = item.Date
+        };
     }
+    //public static IEnumerable<Note> LoadAll()
+    //{
+    //    // Get the folder where the notes are stored.
+    //    string appDataPath = FileSystem.AppDataDirectory;
+
+    //    // Use Linq extensions to load the *.notes.txt files.
+    //    return Directory
+
+    //            // Select the file names from the directory
+    //            .EnumerateFiles(appDataPath, "*.notes.txt")
+
+    //            // Each file name is used to load a note
+    //            .Select(filename => Note.Load(Path.GetFileName(filename)))
+
+    //            // With the final collection of notes, order them by date
+    //            .OrderByDescending(note => note.Date);
+    //}
     public static IEnumerable<Note> LoadAll()
     {
-        // Get the folder where the notes are stored.
-        string appDataPath = FileSystem.AppDataDirectory;
-
-        // Use Linq extensions to load the *.notes.txt files.
-        return Directory
-
-                // Select the file names from the directory
-                .EnumerateFiles(appDataPath, "*.notes.txt")
-
-                // Each file name is used to load a note
-                .Select(filename => Note.Load(Path.GetFileName(filename)))
-
-                // With the final collection of notes, order them by date
-                .OrderByDescending(note => note.Date);
+        var items = new NoteRepository().ReadItems("");
+        return items.Select(item => new Note
+        {
+            Filename = item.Name,
+            Text = item.Data,
+            Date = item.Date
+        });
     }
 }
--- obj/Debug/net9.0-maccatalyst/maccatalyst-x64/Notes.AssemblyInfo.cs
+++ obj/Debug/net9.0-maccatalyst/maccatalyst-x64/Notes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d63ad40fb7ca0b3354a7b4a8f800ae5cfb3b196a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
